7096fbb3f5c157773cd4bea39333cdf56cd97de6
JyotiKumari1277-ux <jyotikumarirs8709@gmail.com>
2026-07-17 05:48:17 +0530

    Fix socket connection to use API_URL for deployment

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
+import { API_URL } from './config';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
-
-const socket = io(SOCKET_URL);
+const socket = io(API_URL);
 
 export default socket;
